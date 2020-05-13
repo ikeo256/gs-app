145a05255d2e738ecc0adca48ada1cbab2d3d55f
ikeo256 <ikeo.hogehoge@gmail.com>
2020-05-13 21:29:46 +0900

    Add function searching books

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,145 @@
 import React, {useState} from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 import {BookList} from './components/index';
+import AppBar from '@material-ui/core/AppBar';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import {fade, makeStyles} from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   const languages = ['React', 'Vue', 'Angular'];
   const [keyword, setKeyword] = useState('react');
+  const [value, setValue] = useState('');
 
   const handleClick = (keyword) => {
     setKeyword(keyword);
   }
 
+  const handleChange = ((e) => {
+    setValue(e.target.value);
+  })
+
   return (
-    <BrowserRouter>
-      <div>
-        <h1>react app</h1>
-        <ul>
-          <li><Link to={'/'} onClick={() => handleClick('react')}>React</Link></li>
-          <li><Link to={'/vue'} onClick={() => handleClick('vue')}>Vue</Link></li>
-          <li><Link to={'/angular'} onClick={() => handleClick('angular')}>Angular</Link></li>
-        </ul>
+    <BrowserRouter className={classes.root}>
+      <CssBaseline/>
+      <AppBar position="relative" color="inherit">
+        <Toolbar>
+          <MenuBookIcon className={classes.icon}/>
+          <Typography variant="h6" className={classes.title} noWrap>
+            Book Lists
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon/>
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              value={value}
+              onChange={handleChange}
+            />
+          </div>
+          <Link
+            to={'/'}
+            onClick={() => handleClick('react')}
+            className={classes.link}
+          >
+            <Button variant="outlined" color="primary">React</Button>
+          </Link>
+          <Link
+            to={'/vue'}
+            onClick={() => handleClick('vue')}
+            color="inherit"
+            className={classes.link}
+          >
+            <Button variant="outlined">Vue</Button>
+          </Link>
+          <Link
+            to={'/angular'}
+            onClick={() => handleClick('angular')}
+            color="inherit"
+            className={classes.link}
+          >
+            <Button variant="outlined" color="secondary">Angular</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+
+      <main>
         <Route
           exact path={'/'}
           render={
             () => <BookList
               language={languages[0]}
               keyword={keyword}
+              value={value}
             />
           }
         />
@@ -33,6 +148,7 @@ const App = () => {
           render={() => <BookList
             language={languages[1]}
             keyword={keyword}
+            value={value}
           />}
         />
         <Route
@@ -40,9 +156,10 @@ const App = () => {
           render={() => <BookList
             language={languages[2]}
             keyword={keyword}
+            value={value}
           />}
         />
-      </div>
+      </main>
     </BrowserRouter>
   )
 }
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,31 +1,122 @@
 import React, {useState, useEffect} from 'react';
+import {Loading} from './index';
 import axios from "axios";
+import {makeStyles} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
 
 const BookList = (props) => {
-    const [bookData, setBookData] = useState(null);
+  const classes = useStyles();
+  const [bookData, setBookData] = useState({});
+  const [firstData, setFirstData] = useState([]);
+  const [formatData, setFormatdata] = useState([]);
 
-    useEffect(() => {
-        (async () => {
-            const requestUrl = 'https://www.googleapis.com/books/v1/volumes?q=intitle:'
-            await axios.get(`${requestUrl}${props.keyword}`)
-                .then((response) => {
-                    setBookData(response);
-                })
-        })();
-    }, [props.keyword])
+  // ページ切替
+  useEffect(() => {
+    (async () => {
+      const requestUrl = 'https://www.googleapis.com/books/v1/volumes?q=intitle:'
+      await axios.get(`${requestUrl}${props.keyword}`)
+        .then((response) => {
+          setBookData(response);
 
-    return (
-        <div>
-            <ul>
-                {(bookData === null)
-                    ? <p>now loading...</p>
-                    : bookData.data.items.map((value, index) => {
-                        return <li key={index.toString()}>{value.volumeInfo.title}</li>
-                    })
-                }
-            </ul>
-        </div>
-    )
+          // 必要なデータを配列に代入
+          if (Object.keys(response).length > 0) {
+            const array = [];
+            response.data.items.map((value, index) => {
+              const thumbnail = (Object.keys(value.volumeInfo).includes('imageLinks'))
+                ? value.volumeInfo.imageLinks.thumbnail
+                : "";
+              const obj = {
+                title: value.volumeInfo.title,
+                thumbnail: thumbnail,
+                infoLink: value.volumeInfo.infoLink
+              };
+              array.push(obj);
+            })
+
+            setFirstData(array);
+            setFormatdata(array);
+          }
+        })
+    })();
+  }, [props.keyword])
+
+  // 検査
+  useEffect(() => {
+    let result = [];
+
+    if (Object.keys(bookData).length > 0) {
+      const formatArray = [];
+      bookData.data.items.map((value, index) => {
+        const thumbnail = (Object.keys(value.volumeInfo).includes('imageLinks'))
+          ? value.volumeInfo.imageLinks.thumbnail
+          : "";
+        const obj = {
+          title: value.volumeInfo.title,
+          thumbnail: thumbnail,
+          infoLink: value.volumeInfo.infoLink
+        };
+        formatArray.push(obj);
+      })
+      setFormatdata(formatArray);
+
+      // 正規表現であいまい検索
+      result = formatArray.filter(object => {
+        const reg = new RegExp(props.value, 'i');
+        return object.title.match(reg);
+      })
+    }
+
+    if (props.value === '') {
+      setFormatdata(firstData);
+    } else {
+      setFormatdata(result);
+    }
+  }, [props.value])
+
+  return (
+    <>
+      {(Object.keys(bookData).length === 0) ? (
+        <Loading/>
+      ) : (
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={6}>
+            {formatData.map((value, index) => (
+              <Grid item key={index.toString()} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <a href={value.infoLink} target="_blank" rel="noopener">
+                    <CardMedia
+                      component="img"
+                      title={value.title}
+                      src={value.thumbnail}
+                      alt={value.title}
+                    />
+                  </a>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      )}
+    </>
+  );
 }
 
 export default BookList;
